f5d512b2b7a0a4cb960046519ab35f73e4f48267
barakatimothy <timothybaraka39@gmail.com>
2023-07-20 17:06:36 -0400

    complete!

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,16 +7,35 @@ function Counter() {
     setCount(count + 1);
   }
 
-  const counterStyle = {
-    color: "green",
-    fontSize: "240px", 
-    cursor: "pointer",
-    alignItems:"center",
-    justifyContent:"center",
-    
-  };
-
-  return <div style={counterStyle} onClick={increment}>{count}</div>;
+  return <button onClick={increment}>{count}</button>;
 }
 
 export default Counter;
+
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//   }
+
+//   const counterStyle = {
+//     color: "green",
+//     fontSize: "240px", 
+//     cursor: "pointer",
+//     alignItems:"center",
+//     justifyContent:"center",
+    
+//   };
+
+//   return <div style={counterStyle} onClick={increment}>{count}</div>;
+// }
+
+
+
+
+// function increment() {
+//   setCount((currentCount) => currentCount + 1);
+//   setCount((currentCount) => currentCount + 1);
+// }
